3aac7be58b8991eb7dfc124b2cc72df8fa351cec
Jonathan Grieve <mail@jonniegrieve.co.uk>
2021-09-10 12:05:43 +0100

    define db schema

---
+++ models.py
@@ -0,0 +1,28 @@
+ # Declare imports for the app
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+import datetime
+
+
+# Connect to SQLAlchemy
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///roster.db'
+
+app.secret_key = "gggd465dfgghffhehshy"  # Secret key
+
+
+db = SQLAlchemy(app)
+
+
+# Create Database Model - 3 fields + ID as primary key
+class Roster(db.Model):
+    id = db.Column(db.Integer(), primary_key=True)
+    name = db.Column("Name", db.String(80), unique=True, nullable=False)
+    age = db.Column("Age", db.Integer, unique=True, nullable=False)
+    joined = db.Column('Joined', db.DateTime, default=datetime.datetime.now)
+
+    def __repr__(self):
+        return f'''<Roster %r> ({self.name} 
+        Age: {self.age}
+        Joined: {self.joined }
+       '''
